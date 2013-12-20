26f61129b2b40fc4cc65b24cf9ab6ead177b5e83
Mark Backsmeier <mbacksmeier@gmail.com>
2013-12-19 22:40:03 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Laborsight::Application.config.secret_key_base = 'e9f766a4f858077955849207d85efe7d6515cde00a6f97239019b38cdc69e9bffe22406a44c1f4caa1c893c416ca7933ce27c5299042d1a59e59e3b17612fd47'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Laborsight::Application.config.session_store :cookie_store, key: '_laborsight_session'
