66ae10ab88fb22f8979a9a7f5fb8c7caa267ec7d
Katherine Champagne <kechampagne@gmail.com>
2014-01-25 01:36:28 -0500

    Testing API calls from index controller

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,9 +1,16 @@
 class MapsController < ApplicationController
+  include Dol 
   def index
   	# if cookies[:disclaimer]
   	# else
   	# 	flash[:notice] = "DISCLAIMER"
   	# end
   	# cookies[:disclaimer] = true
+  	context = GOV::DataContext.new(SECRETS['gov']['api_host'], SECRETS['gov']['api_key'], SECRETS['gov']['api_secret'], SECRETS['gov']['api_data'], SECRETS['gov']['api_uri'])
+  	request = GOV::DataRequest.new context
+    @results = test_call_two(request)
+    puts @results
   end
+
+
 end
