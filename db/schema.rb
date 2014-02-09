1f7860d8152a90d1d9afa9f98821486ba372c5a6
Katherine Champagne <kechampagne@gmail.com>
2014-02-09 14:51:54 -0500

    Add latitude and longitude to Company table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140125034321) do
+ActiveRecord::Schema.define(version: 20140209194533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20140125034321) do
     t.date     "load_date"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
 end
